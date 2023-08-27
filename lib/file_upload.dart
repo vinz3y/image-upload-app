b9131cac6195452352b6ea5550f0075ca2f69fa9
Vinura Perera <vinuraperera2@gmail.com>
2023-08-28 03:36:29 +0530

    New UI Design updated

--- lib/file_upload.dart
+++ lib/file_upload.dart
@@ -40,10 +40,10 @@ class _ImageUploadPageState extends State<ImageUploadPage> {
 
       if (response.statusCode == 200) {
         // Image uploaded successfully
-        print('Image uploaded successfully');
+        print('Image/GIF uploaded successfully');
       } else {
         // Error uploading image
-        print('Error uploading image');
+        print('Error uploading Image/GIF');
       }
     }
   }
@@ -51,96 +51,52 @@ class _ImageUploadPageState extends State<ImageUploadPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(15, 15, 15, 1),
       appBar: AppBar(
-        title: Text('Image Upload App'),
+        title: Text('Image Upload App',
+            style: TextStyle(color: Color.fromARGB(235, 255, 255, 255))),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             SizedBox(
-              width: 150,
-              height: 50,
+              width: 100,
+              height: 35,
             ),
             SizedBox(
-                width: 200,
                 height: 30,
                 child: Container(
+                  height: 300,
+                  width: 300,
                   alignment: Alignment.topCenter,
                   child: Text(
                     "Image Preview",
                     style: TextStyle(
-                      fontSize: 18, // Set the font size
-                      fontWeight: FontWeight.bold,
-                    ),
+                        fontSize: 18, // Set the font size
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(235, 185, 142, 255)),
                   ),
                 )),
+            SizedBox(
+              height: 40,
+            ),
             (_imageFile != null)
                 ? Image.file(
                     _imageFile!,
-                    height: 200,
-                    width: 200,
+                    height: 250,
+                    width: 250,
                     fit: BoxFit.contain,
                   )
                 : Image.asset(
                     'assets/icons/preview.png', // Path to default image asset
-                    height: 200,
-                    width: 200,
+                    height: 250,
+                    width: 250,
                     fit: BoxFit.cover,
                   ),
             SizedBox(
               width: 100,
-              height: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ButtonTheme(
-                  minWidth: 200.0,
-                  height: 400.0,
-                  child: ElevatedButton.icon(
-                    onPressed: () => _pickImage(ImageSource.gallery),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(
-                          240, 248, 221, 255), // Background color
-                    ),
-                    label: Text(
-                      'Pick Image(JPG/JPEG/PNG)',
-                      style: TextStyle(
-                        color: const Color.fromARGB(
-                            255, 36, 20, 65), // Set the text color here
-                        fontSize: 12, // Set the font size
-                      ),
-                    ),
-                    icon: Image.asset('assets/icons/image.png',
-                        width: 40, height: 40),
-                  ),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                ElevatedButton.icon(
-                  onPressed: () => _pickImage(ImageSource.gallery),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        Color.fromARGB(240, 248, 221, 255), // Background color
-                  ),
-                  label: Text(
-                    'Pick GIF',
-                    style: TextStyle(
-                      color: const Color.fromARGB(
-                          255, 36, 20, 65), // Set the text color here
-                      fontSize: 12, // Set the font size
-                    ),
-                  ),
-                  icon: Image.asset('assets/icons/gif.png',
-                      width: 40, height: 40),
-                ),
-              ],
-            ),
-            SizedBox(
-              width: 100,
-              height: 50,
+              height: 60,
             ),
 
             //Changing Part
@@ -153,7 +109,11 @@ class _ImageUploadPageState extends State<ImageUploadPage> {
                   child: ElevatedButton(
                       onPressed: () => _pickImage(ImageSource.gallery),
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(240, 248, 221, 255),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              10), // Set the border radius
+                        ),
+                        backgroundColor: Color.fromARGB(235, 235, 225, 250),
                         padding: EdgeInsets.all(
                             16), // Set padding around the content
                       ),
@@ -193,7 +153,11 @@ class _ImageUploadPageState extends State<ImageUploadPage> {
                   child: ElevatedButton(
                       onPressed: () => _pickImage(ImageSource.gallery),
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(240, 248, 221, 255),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              10), // Set the border radius
+                        ),
+                        backgroundColor: Color.fromARGB(235, 235, 225, 250),
                         padding: EdgeInsets.all(
                             16), // Set padding around the content
                       ),
@@ -230,17 +194,23 @@ class _ImageUploadPageState extends State<ImageUploadPage> {
               width: 100,
               height: 20,
             ),
-            SizedBox(
-              width: 100,
-              height: 20,
-            ),
+
             ElevatedButton(
               onPressed: _uploadImage,
               style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(10), // Set the border radius
+                ),
+                fixedSize: Size(125, 45),
                 backgroundColor:
-                    Color.fromARGB(255, 26, 15, 29), // Background color
+                    Color.fromARGB(235, 122, 39, 255), // Background color
               ),
-              child: Text('Upload (Image/GIF)'),
+              child: Column(children: [
+                SizedBox(height: 5),
+                Text('Upload'),
+                Text('(Image/GIF)')
+              ]),
             ),
           ],
         ),
